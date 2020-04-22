b731b7399ea7d3295027112aee2c920c98b09ee1
NhatQuang <nhatquang.neu@gmail.com>
2020-04-22 14:45:19 +0100

    Initial Commit

---
+++ SoundBBC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundBBC
+//
+//  Created by nhatquangz on 4/22/20.
+//  Copyright © 2020 nhatquangz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
